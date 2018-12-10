5053ab21dec352b50cc1416bdbccd4400c1f7919
Yansen Dou <sendy_dou@hotmail.com>
2018-12-10 19:30:17 +0800

    [@ysdou] #n/a add zip and immutableMap

--- src/main/scala/chapter4_map_tuple/Quiz.scala
+++ src/main/scala/chapter4_map_tuple/Quiz.scala
@@ -1,6 +1,16 @@
 package chapter4_map_tuple
 
 object Quiz {
+  private var immutableResult = Map[String, Int]()
+  def countWordByImmutable(word: String): Unit = {
+    if(immutableResult.contains(word)) {
+      immutableResult = immutableResult + (word -> (immutableResult(word) + 1))
+    } else {
+      immutableResult = immutableResult + (word -> 1)
+    }
+  }
+  def getImmutableResult = immutableResult
+
   private val result = scala.collection.mutable.Map[String, Int]()
   def countWord(word: String) = {
     if (result.contains(word)) {
--- src/test/scala/chapter4_map_tuple/QuizTest.scala
+++ src/test/scala/chapter4_map_tuple/QuizTest.scala
@@ -30,4 +30,23 @@ class QuizTest extends FunSuite with DiagrammedAssertions {
     assert(worldCounts("world") == 1)
   }
 
+  test("should be able to count words by immutable map") {
+    val filePath = "src/test/scala/chapter4_map_tuple/input.txt"
+    val in = new Scanner(new java.io.File(filePath))
+    while(in.hasNext()){
+      val oneLine = in.next()
+      Quiz.countWordByImmutable(oneLine)
+    }
+
+    val worldCounts = Quiz.getImmutableResult
+    assert(worldCounts("Hello") == 2)
+    assert(worldCounts("world") == 1)
+  }
+
+  test("show zip operation") {
+    val countries = Array("China", "USA", "India")
+    val popular = Array(12, 5, 13)
+    assert(countries.zip(popular) sameElements Array("China" -> 12, "USA" -> 5, "India" -> 13))
+  }
+
 }
